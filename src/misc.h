cbc7c6619714ec01900ef6c7a1b2c914406e56f3
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-23 11:30:21 +0200

    Added `list_to_*()` to misc.h

--- src/misc.h
+++ src/misc.h
@@ -4,6 +4,8 @@
 #include <RcppArmadillo.h>
 
 // Function declaration
+arma::vec list_to_vector(Rcpp::List r_list);
 arma::mat list_to_matrix(Rcpp::List r_list);
+arma::cube list_to_cube(Rcpp::List r_list);
 
 #endif
