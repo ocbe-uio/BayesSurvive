6f06f31bd6034c8992c9efadf4049368f6718566
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-04-08 15:59:44 +0200

    Removed commented code and FIXMEs (#27)

--- src/func_MCMC_graph_cpp.cpp
+++ src/func_MCMC_graph_cpp.cpp
@@ -2,25 +2,17 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 
 arma::mat construct_G_MRF(arma::mat G_MRF, arma::vec b, unsigned int S, unsigned int p, bool MRF_2b) {
-  // arma::mat G_MRF = (G.n_rows, G.n_cols);
   if (MRF_2b) {
     // TODO: develop test for this case. Off-by-one minefield!
     // two different values for b in MRF prior for subgraphs G_ss and G_rs
     arma::uvec p_seq = arma::regspace<arma::uvec>(0, p - 1);
     for (unsigned int g = 0; g < S; g++) {
-      // b1 * G_ss
-      // arma::uvec g_seq(p, arma::fill::value(g * p));
-      // arma::uvec idx = g_seq + p_seq;
       arma::uvec idx = p_seq + g * p;
       G_MRF.submat(idx, idx) *= b(0);
     }
     for (unsigned int g = 0; g < S - 1; g++) {
       // b2 * G_rs
       for (unsigned int r = g + 1; r < S; r++) {
-        // arma::uvec g_seq(p, arma::fill::value(g * p));
-        // arma::uvec r_seq(p, arma::fill::value(r * p));
-        // arma::uvec idx = g_seq + p_seq;
-        // arma::uvec idx2 = r_seq + p_seq;
         arma::uvec idx = p_seq + g * p;
         arma::uvec idx2 = p_seq + r * p;
         G_MRF.submat(idx, idx2) *= b(1);
@@ -52,11 +44,10 @@ arma::vec randMvNormal(const arma::vec &m, const arma::mat &Sigma) {
     if( eig_sym(eigval, eigvec, Sigma) ) {
       res = eigvec * arma::diagmat(arma::sqrt(eigval)) * Rcpp::as<arma::vec>(Rcpp::rnorm(d));
     } else {
-      // res.fill(0.);
       Rcpp::stop("randMvNorm failing because of singular Sigma matrix.");
     }
   }
-  
+
   return res + m;
 }
 
@@ -86,8 +77,6 @@ Rcpp::List func_MCMC_graph_cpp(
   Rcpp::List Sig = Rcpp::as<Rcpp::List>(ini["Sig.ini"]);
   Rcpp::List C = Rcpp::as<Rcpp::List>(ini["C.ini"]);
 
-  // Rcpp::List gamma_ini_list = Rcpp::as<Rcpp::List>(ini["gamma.ini"]);
-  // arma::vec gamma_ini = Rcpp::as<arma::vec>(gamma_ini_list[0]); // FIXME: use list_to_matrix() and change with S <== Fixed by George as follows
   arma::vec gamma_ini(p * S); // vectorize/unlist ini["gamma.ini"]
   std::size_t position = 0;
   for (unsigned int g = 0; g < S; g++) {
@@ -103,7 +92,7 @@ Rcpp::List func_MCMC_graph_cpp(
     double b_val = Rcpp::as<double>(hyperpar["b"]);
     b.fill(b_val);
   }
-  arma::mat G_MRF = G; 
+  arma::mat G_MRF = G;
   G_MRF = construct_G_MRF(G, b, S, p, MRF_2b);
 
   // Update of precision matrix and graph within each subgroup
@@ -113,9 +102,6 @@ Rcpp::List func_MCMC_graph_cpp(
     arma::mat V_g = V[g];
     arma::mat C_g = C[g];
 
-    // arma::uvec p_seq = arma::regspace<arma::uvec>(0, p - 1);
-    // arma::uvec g_seq(p, arma::fill::value(g * p));
-    // arma::uvec idx = g_seq + p_seq;
     arma::uvec idx = arma::regspace<arma::uvec>(g * p, (g + 1) * p - 1);
     arma::mat G_g = G.submat(idx, idx);
     unsigned int n_g = Rcpp::as<unsigned int>(n[g]);
@@ -126,7 +112,6 @@ Rcpp::List func_MCMC_graph_cpp(
       arma::uvec ind_noi = arma::regspace<arma::uvec>(0, p - 1);
       ind_noi.shed_row(i);
       arma::vec v_temp = V_g.submat(ind_noi, arma::uvec({i}));
-      // v_temp.elem(arma::find(v_temp < 1.0e-10)).fill(1.0e-10);
 
       arma::mat Sig11 = Sig_g.submat(ind_noi, ind_noi);
       arma::vec Sig12 = Sig_g.submat(ind_noi, arma::uvec({i}));
@@ -138,27 +123,16 @@ Rcpp::List func_MCMC_graph_cpp(
       arma::mat Ci = (S_g(i, i) + lambda) * invC11 + arma::diagmat(1. / v_temp);
 
       Ci = 0.5 * (Ci + Ci.t());
-      // arma::mat Ci_chol = arma::chol(Ci);
 
-      // arma::vec mu_i = -arma::solve(Ci_chol, arma::solve(Ci_chol.t(), S_g.submat(ind_noi, arma::uvec({i}))));
-      // // arma::mat beta = mu_i + arma::solve(Ci_chol, arma::randn<arma::vec>(p - 1));
-      // arma::vec beta = mu_i + arma::solve(Ci_chol, Rcpp::as<arma::vec>(Rcpp::rnorm(p - 1)));
-
-      // arma::vec mu_i = -arma::inv_sympd(Ci, arma::inv_opts::allow_approx) * S_g.submat(ind_noi, arma::uvec({i})); 
-      // // using inverse directly instead of chol() & solve() by Madjar
       arma::mat invCi;
       arma::vec mu_i, beta;
       if ( arma::inv_sympd(invCi, Ci) ) {
         mu_i = - invCi * S_g.submat(ind_noi, arma::uvec({i}));
-        beta = randMvNormal(mu_i, Ci); 
+        beta = randMvNormal(mu_i, Ci);
       } else {
-        // arma::inv(invCi, Ci + 0.1 * arma::eye(p - 1, p - 1), arma::inv_opts::allow_approx);
-        // arma::inv(invCi, Ci);
         arma::inv(invCi, Ci, arma::inv_opts::allow_approx);
         mu_i = - invCi * S_g.submat(ind_noi, arma::uvec({i}));
-        // beta = randMvNormal(mu_i, arma::diagmat(Ci)); // TODO: test this!
         beta = mu_i;
-        // if (beta.has_nan()) beta = mu_i;
       }
 
       // Update of last column in Omega_gg
@@ -167,7 +141,6 @@ Rcpp::List func_MCMC_graph_cpp(
 
       double a_gam = 0.5 * n_g + 1;
       double b_gam = (S_g(i, i) + lambda) * 0.5;
-      // double gam = arma::as_scalar(arma::randg(1, arma::distr_param(a_gam, 1 / b_gam)));
       double gam = R::rgamma( a_gam, 1. / b_gam );
 
       double c = arma::as_scalar(beta.t() * invC11 * beta);
